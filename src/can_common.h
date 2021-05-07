7023b666ddf9ef6fa4811557f274fc645e1adeee
Silviu Toderita <silviu.toderita@gmail.com>
2021-05-07 16:38:28 -0700

    Include telemetry IDs. (#2)
    * Update can_common.h
    
    -Added default telemetry ID of 0xA0 (160)
    -Added BMS REQUEST and RETURN IDs assuming BMS Node ID is default 0x01
    
    * Update can_common.h
    
    * Update can_common.h
    
    Changed define statements to macros

--- src/can_common.h
+++ src/can_common.h
@@ -1,11 +1,13 @@
 #ifndef _CAN_COMMON_H_
 #define _CAN_COMMON_H_
 
-#define CAN_THROTTLE 45
-#define CAN_MOTOR_ENABLE 20
+#define CAN_THROTTLE        45
+#define CAN_MOTOR_ENABLE    20
 #define CAN_ACCESSORIES
-#define CAN_TELEMETRY
+#define CAN_TELEMETRY       0xA0
 
-#define CAN_BMS // TODO we have to split these out
+#define CAN_BMS_DEFAULT_ID  0x01
+#define CAN_BMS_REQUEST(x)  (0x200 + (x))
+#define CAN_BMS_RETURN(x)   (0x240 + (x))
 
 #endif
