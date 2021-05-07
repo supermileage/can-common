208182e30b51188d16483bc59f022a4f9426e8b4
Silviu Toderita <silviu.toderita@gmail.com>
2021-05-07 15:48:52 -0700

    Update can_common.h

--- src/can_common.h
+++ src/can_common.h
@@ -1,13 +1,13 @@
 #ifndef _CAN_COMMON_H_
 #define _CAN_COMMON_H_
 
-#define CAN_THROTTLE      45
-#define CAN_MOTOR_ENABLE  20
+#define CAN_THROTTLE        45
+#define CAN_MOTOR_ENABLE    20
 #define CAN_ACCESSORIES
-#define CAN_TELEMETRY     0xA0
+#define CAN_TELEMETRY       0xA0
 
-// Assuming default BMS node ID of 0x01
-#define CAN_BMS_REQUEST   0x201
-#define CAN_BMS_RETURN    0x241
+#define CAN_BMS_DEFAULT_ID  0x01
+#define CAN_BMS_REQUEST     0x200 + CAN_BMS_DEFAULT_ID
+#define CAN_BMS_RETURN      0x240 + CAN_BMS_DEFAULT_ID
 
 #endif
