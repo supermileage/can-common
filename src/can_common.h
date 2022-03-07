f1c4c0d1d705b76401fd7f27a8d4e4a5d6274ab6
Silviu Toderita <silviu.toderita@gmail.com>
2022-03-06 18:28:52 -0800

    Updates comments to make clear which IDs are TX/RX by which module. Renames some constants for clarity. Removes CAN Accessories statuses.

--- src/can_common.h
+++ src/can_common.h
@@ -4,33 +4,31 @@
 // Steering
 // 0x0 - 0x5F
 // 0x100 - 0x15F
-#define STEERING_THROTTLE   0x01
-#define STEERING_STATUS     0x02
+//      TX
+#define CAN_STEERING_THROTTLE   0x01
+#define CAN_STEERING_READY      0x02
 
 // Accessories
 // 0x60 - 0x7F
 // 0x160 - 0x17F
-#define CAN_ACC_OPERATION       0x60
+//      TX
 #define CAN_ACC_STATUS          0x71
-
-#define STATUS_HEADLIGHTS		0x0
-#define STATUS_BRAKELIGHTS		0x1
-#define STATUS_HORN				0x2
-#define STATUS_HAZARDS			0x3
-#define STATUS_RIGHT_SIGNAL		0x4
-#define STATUS_LEFT_SIGNAL		0x5
-#define STATUS_WIPERS			0x6
-#define STATUS_NULL				0xFF
+//      RX
+#define CAN_ACC_OPERATION       0x60
 
 // Telemetry
 // 0x80 - 0x9F
 // 0x180 - 0x19F
+//      TX
 #define CAN_TELEMETRY_GPS_SPEED 0x80
 
-// BMS
+// TinyBMS
 // 0x201 - 0x23F
 // 0x241 - 0x27F
-#define CAN_BMS_REQUEST         0x201
-#define CAN_BMS_RESPONSE        0x241
+//      TX
+#define CAN_TINYBMS_RESPONSE        0x241
+//      RX
+#define CAN_TINYBMS_REQUEST         0x201
+
 
 #endif
